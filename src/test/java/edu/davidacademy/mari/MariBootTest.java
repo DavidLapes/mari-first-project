24b9dc69d7d142aeb162e2478b26f88ae98885c6
David Lapeš <dave.lapes@gmail.com>
2024-03-16 16:10:01 +0100

    Generate Spring Boot project

---
+++ src/test/java/edu/davidacademy/mari/MariBootTest.java
@@ -0,0 +1,13 @@
+package edu.davidacademy.mari;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MariBootTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
